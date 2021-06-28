79cf976b5dfe32aedc5e5e9228e26a170531f40e
Bachir-Ndiaye <amadou.n-diaye@hotmail.com>
2021-06-28 14:09:02 +0200

    added discount entity + fixtures

---
+++ src/DataFixtures/DiscountFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Discount;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class DiscountFixtures extends Fixture
+{
+    const DISCOUNT_TYPE = ['flash','oneweek'];
+
+    public function load(ObjectManager $manager)
+    {
+       for($i = 0; $i < sizeof(self::DISCOUNT_TYPE); $i++){
+
+           $discount = new Discount();
+           $discount->setType(self::DISCOUNT_TYPE[$i]);
+
+           $manager->persist($discount);
+       }
+
+       $manager->flush();
+    }
+}
